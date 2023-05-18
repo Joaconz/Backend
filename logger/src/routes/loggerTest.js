3f858bc02328c8b3ead2b5574b9aaa763f0471c5
Joaquin Nunez <joaquinunez2004@gmail.com>
2023-05-18 17:50:09 -0300

    logger

---
+++ logger/src/routes/loggerTest.js
@@ -0,0 +1,16 @@
+import { Router } from "express";
+
+const router = Router()
+
+router.get('/', async (req, res)=>{
+    // req.logger.fatal("Fatal")
+    // req.logger.error("Error")
+    // req.logger.warning("Warning")
+    // req.logger.info("Info")
+    // req.logger.http("HTTP")
+    // req.logger.debug("Debug")
+    req.logger.warning("warn")
+    res.send({message: "Prueba Logger"})
+})
+
+export default router
---
+++ logger/src/utils/logger.js
@@ -0,0 +1,81 @@
+import winston from "winston";
+import configObject from "../config/config.js";
+
+const customLevelOptions = {
+    levels: {
+        fatal: 0,
+        error: 1,
+        warning: 2,
+        info: 3,
+        http: 4,
+        debug: 5
+    },
+    colors: {
+        fatal: "red",
+        error: "orange",
+        warning: "yellow",
+        info: "blue",
+        http: "green",
+        debug: "white",
+    }
+}
+
+// const logger = winston.createLogger({
+//     levels: customLevelOptions,
+//     format: winston.format.combine(
+//         winston.format.colorize({colors: customLevelOptions.colors}),
+//         winston.format.simple()
+//     ),
+//     transports: [
+//         new winston.transports.Console({level: "http"}),
+//         new winston.transports.File({filename: './errors.log', level: 'error'}),
+//     ]
+// })
+
+const devLogger = winston.createLogger({
+    levels: customLevelOptions.levels,
+    transports: [
+        new winston.transports.Console({
+            level: "http",
+            format: winston.format.combine(
+                winston.format.colorize({colors: customLevelOptions.colors}),
+                winston.format.simple()
+            )
+        }),
+        new winston.transports.File({
+            filename: './errors.log', 
+            level: 'error',
+            format: winston.format.simple()
+        }),
+        new winston.transports.Console({level: "http"})
+    ]
+})
+
+// const prodLogger = winston.createLogger({
+//     levels: customLevelOptions,
+//     format: winston.format.combine(
+//         winston.format.colorize({colors: customLevelOptions.colors}),
+//         winston.format.simple()
+//     ),
+//     transports: [
+//         new winston.transports.Console({
+//             level: "info",
+//             format: winston.format.combine(
+//                 winston.format.colorize({colors: customLevelOptions.colors}),
+//                 winston.format.simple()
+//             )
+//         }),
+//         new winston.transports.File({
+//             filename: './errors.log', 
+//             level: 'error',
+//             format: winston.format.simple()
+//         }),
+//     ]
+// })
+
+export const addLogger = (req, res, next) => {
+    // req.logger = configObject.mode === 'production' ? prodLogger : devLogger
+    req.logger = devLogger
+    req.logger.http(`${req.method} en ${req.url} - ${new Date().toLocaleTimeString}`)
+    next();
+}
